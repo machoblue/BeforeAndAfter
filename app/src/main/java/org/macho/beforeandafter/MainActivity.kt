0c59dc66f33cfd27e8dde69f95f408ac0cf0d416
松島勇貴 <macho.macho.dev@gmail.com>
2019-10-26 22:00:34 +0900

    Add BottomNav

--- app/src/main/java/org/macho/beforeandafter/MainActivity.kt
+++ app/src/main/java/org/macho/beforeandafter/MainActivity.kt
@@ -1,12 +1,7 @@
 package org.macho.beforeandafter
 
 import android.content.Intent
-import android.graphics.Color
 import android.os.Bundle
-import android.support.v4.content.ContextCompat
-import android.view.View
-import android.widget.ImageButton
-import android.widget.TextView
 import com.google.android.gms.ads.MobileAds
 import dagger.android.support.DaggerAppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
@@ -19,13 +14,6 @@ import javax.inject.Inject
 
 class MainActivity: DaggerAppCompatActivity() {
 
-    private var selectedImageButton: ImageButton? = null
-    private var selectedTextView: TextView? = null
-
-    private var colorSelected = 0
-
-//    @Inject
-//    lateinit var recordFragmentProvider: Lazy<RecordFragment>
     @Inject
     lateinit var recordFragment: RecordFragment
 
@@ -33,25 +21,33 @@ class MainActivity: DaggerAppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        colorSelected = ContextCompat.getColor(this, R.color.colorBottomNaviItemPressed)
+        bottomNavigation.setOnNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.records -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.content, recordFragment).commit()
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.gallery -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.content, GalleryFragment.getInstance()).commit()
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.graphe -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.content, GrapheFragment.getInstance()).commit()
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.settings -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.content, PreferenceFragment.newFragment(this)).commit()
+                    return@setOnNavigationItemSelectedListener true
+                }
+                else -> {
+                    return@setOnNavigationItemSelectedListener false
+                }
+            }
+        }
 
-        uncheckCurrentButtonAndCheck(item0ImageButton, item0TextView)
-
-//        supportFragmentManager.beginTransaction().replace(R.id.content, recordFragmentProvider.get()).commit()
         supportFragmentManager.beginTransaction().replace(R.id.content, recordFragment).commit()
 
         configureAd()
-    }
-
-    private fun uncheckCurrentButtonAndCheck(imageButton: ImageButton, textView: TextView) {
-        selectedImageButton?.clearColorFilter()
-        selectedTextView?.setTextColor(Color.rgb(255, 255, 255))
-
-        selectedImageButton = imageButton
-        selectedTextView = textView
-
-        selectedImageButton?.setColorFilter(colorSelected)
-        selectedTextView?.setTextColor(colorSelected)
     }
 
     private fun configureAd() {
@@ -60,28 +56,6 @@ class MainActivity: DaggerAppCompatActivity() {
         MobileAds.initialize(this, getString(R.string.admob_app_id))
 
         AdUtil.loadBannerAd(adView, applicationContext)
-    }
-
-    fun onItem0Click(view: View) {
-        uncheckCurrentButtonAndCheck(item0ImageButton, item0TextView)
-
-//        supportFragmentManager.beginTransaction().replace(R.id.content, recordFragmentProvider.get()).commit()
-        supportFragmentManager.beginTransaction().replace(R.id.content, recordFragment).commit()
-    }
-
-    fun onItem1Click(view: View) {
-        uncheckCurrentButtonAndCheck(item1ImageButton, item1TextView)
-        supportFragmentManager.beginTransaction().replace(R.id.content, GalleryFragment.getInstance()).commit()
-    }
-
-    fun onItem2Click(view: View) {
-        uncheckCurrentButtonAndCheck(item2ImageButton, item2TextView)
-        supportFragmentManager.beginTransaction().replace(R.id.content, GrapheFragment.getInstance()).commit()
-    }
-
-    fun onItem3Click(view: View) {
-        uncheckCurrentButtonAndCheck(item3ImageButton, item3TextView)
-        supportFragmentManager.beginTransaction().replace(R.id.content, PreferenceFragment.newFragment(this)).commit()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
