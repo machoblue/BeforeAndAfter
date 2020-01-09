8a5187e7d42dc16f2172af3c6c49355d10c32959
松島勇貴 <macho.macho.dev@gmail.com>
2020-01-09 23:32:11 +0900

    add passcode layout

--- app/src/main/java/org/macho/beforeandafter/shared/screen/passcode/PasscodeActivity.kt
+++ app/src/main/java/org/macho/beforeandafter/shared/screen/passcode/PasscodeActivity.kt
@@ -1,4 +1,14 @@
 package org.macho.beforeandafter.shared.screen.passcode
 
-class PasscodeActivity {
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import org.macho.beforeandafter.R
+
+class PasscodeActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.passcode_act)
+    }
 }
