df089f31cab4c6811029d1f88f849b6d7aa6a66c
松島勇貴 <macho.macho.dev@gmail.com>
2020-01-15 23:14:38 +0900

    Rewrite scope annotation by kotlin

---
+++ app/src/main/java/org/macho/beforeandafter/shared/di/ActivityScoped.kt
@@ -0,0 +1,8 @@
+package org.macho.beforeandafter.shared.di
+
+import javax.inject.Scope
+
+@MustBeDocumented
+@Scope
+@Retention(AnnotationRetention.RUNTIME)
+annotation class ActivityScoped
---
+++ app/src/main/java/org/macho/beforeandafter/shared/di/FragmentScoped.kt
@@ -0,0 +1,6 @@
+package org.macho.beforeandafter.shared.di
+
+@MustBeDocumented
+@Retention(AnnotationRetention.RUNTIME)
+@Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION)
+annotation class FragmentScoped
