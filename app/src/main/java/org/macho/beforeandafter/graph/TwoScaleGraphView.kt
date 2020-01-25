38dc774cf054f0538ed848ba82f69869b732938c
松島勇貴 <macho.macho.dev@gmail.com>
2020-01-25 22:43:54 +0900

    Add graph axis

--- app/src/main/java/org/macho/beforeandafter/graph/TwoScaleGraphView.kt
+++ app/src/main/java/org/macho/beforeandafter/graph/TwoScaleGraphView.kt
@@ -2,11 +2,22 @@ package org.macho.beforeandafter.graph
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
 import org.macho.beforeandafter.shared.util.LogUtil
 
 class TwoScaleGraphView: View {
+    companion object {
+        const val AXIS_WIDTH = 2f
+    }
+
+    var oX = 0f
+    var oY = 0f
+    var maxX = 0f
+    var maxY = 0f
+
     constructor(context: Context): super(context) {
     }
 
@@ -16,9 +27,34 @@ class TwoScaleGraphView: View {
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int): super(context, attrs, defStyleAttr) {
     }
 
+    val axisPaint = Paint().also {
+        it.color = Color.BLACK
+        it.strokeWidth = AXIS_WIDTH
+//        it.isAntiAlias = false
+        it.style = Paint.Style.STROKE
+    }
+
     override fun draw(canvas: Canvas?) {
         super.draw(canvas)
+
+        if (canvas == null) return
+
         LogUtil.d(this, "width : ${this.width}")
         LogUtil.d(this, "height: ${this.height}")
+
+        oX = 0f
+        oY = height * 0.95f
+        maxX = width.toFloat()
+        maxY = height * 0.05f
+
+        drawAxis(canvas)
+    }
+
+    private fun drawAxis(canvas: Canvas) {
+        val yAxis1X = oX + AXIS_WIDTH / 2
+        canvas.drawLine(yAxis1X, oY, yAxis1X, maxY, axisPaint) // yAxis1
+        val yAxis2X = maxX - AXIS_WIDTH / 2
+        canvas.drawLine(yAxis2X, oY, yAxis2X, maxY, axisPaint) // yAxis1
+        canvas.drawLine(oX, oY, maxX, oY, axisPaint) // xAxis
     }
 }
