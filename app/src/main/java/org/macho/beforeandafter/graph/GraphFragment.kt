ef51eb244f54fbda0099d168ff6d66247cdc7975
松島勇貴 <macho.macho.dev@gmail.com>
2020-01-26 10:00:40 +0900

    Draw lines

--- app/src/main/java/org/macho/beforeandafter/graph/GraphFragment.kt
+++ app/src/main/java/org/macho/beforeandafter/graph/GraphFragment.kt
@@ -5,9 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import dagger.android.support.DaggerFragment
+import kotlinx.android.synthetic.main.graph_frag.*
 import org.macho.beforeandafter.R
+import org.macho.beforeandafter.shared.data.Record
 import org.macho.beforeandafter.shared.data.RecordRepository
 import org.macho.beforeandafter.shared.di.ActivityScoped
+import org.macho.beforeandafter.shared.util.LogUtil
+import java.util.*
 import javax.inject.Inject
 
 @ActivityScoped
@@ -21,5 +25,31 @@ class GraphFragment: DaggerFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        LogUtil.d(this, "onViewCreated")
+
+        val records = mutableListOf<Record>()
+        records.add(Record(Date().time - 1000L * 60 * 60 * 24 * 21, 78f, 20f))
+        records.add(Record(Date().time - 1000L * 60 * 60 * 24 * 20, 78.5f, 20f))
+        records.add(Record(Date().time - 1000L * 60 * 60 * 24 * 19, 78f, 20f))
+        records.add(Record(Date().time - 1000L * 60 * 60 * 24 * 18, 76f, 20f))
+        records.add(Record(Date().time - 1000L * 60 * 60 * 24 * 17, 77f, 20f))
+        records.add(Record(Date().time - 1000L * 60 * 60 * 24 * 16, 78f, 20f))
+        records.add(Record(Date().time - 1000L * 60 * 60 * 24 * 15, 79f, 20f))
+        records.add(Record(Date().time - 1000L * 60 * 60 * 24 * 14, 75f, 20f))
+        records.add(Record(Date().time - 1000L * 60 * 60 * 24 * 13, 74f, 20f))
+        records.add(Record(Date().time - 1000L * 60 * 60 * 24 * 12, 75f, 20f))
+        records.add(Record(Date().time - 1000L * 60 * 60 * 24 * 11, 75f, 20f))
+        records.add(Record(Date().time - 1000L * 60 * 60 * 24 * 10, 76f, 20f))
+        records.add(Record(Date().time - 1000L * 60 * 60 * 24 * 9, 74f, 20f))
+        records.add(Record(Date().time - 1000L * 60 * 60 * 24 * 8, 76f, 20f))
+        records.add(Record(Date().time - 1000L * 60 * 60 * 24 * 7, 75f, 20f))
+        records.add(Record(Date().time - 1000L * 60 * 60 * 24 * 6, 76f, 20f))
+        records.add(Record(Date().time - 1000L * 60 * 60 * 24 * 5, 76f, 20f))
+        records.add(Record(Date().time - 1000L * 60 * 60 * 24 * 4, 75f, 20f))
+        records.add(Record(Date().time - 1000L * 60 * 60 * 24 * 3, 75f, 20f))
+        records.add(Record(Date().time - 1000L * 60 * 60 * 24 * 2, 74f, 20f))
+        records.add(Record(Date().time - 1000L * 60 * 60 * 24 * 1, 74f, 20f))
+        records.add(Record(Date().time - 1000L * 60 * 60 * 24 * 0, 73f, 20f))
+        graphView.records = records
     }
 }
--- app/src/main/java/org/macho/beforeandafter/graph/TwoScaleGraphView.kt
+++ app/src/main/java/org/macho/beforeandafter/graph/TwoScaleGraphView.kt
@@ -6,11 +6,15 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
+import org.macho.beforeandafter.shared.data.Record
 import org.macho.beforeandafter.shared.util.LogUtil
+import kotlin.math.ceil
+import kotlin.math.floor
 
 class TwoScaleGraphView: View {
     companion object {
         const val AXIS_WIDTH = 2f
+        const val LINE_WIDTH = 1f
     }
 
     var oX = 0f
@@ -27,6 +31,8 @@ class TwoScaleGraphView: View {
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int): super(context, attrs, defStyleAttr) {
     }
 
+    var records: List<Record> = mutableListOf()
+
     val axisPaint = Paint().also {
         it.color = Color.BLACK
         it.strokeWidth = AXIS_WIDTH
@@ -34,8 +40,15 @@ class TwoScaleGraphView: View {
         it.style = Paint.Style.STROKE
     }
 
+    val linePaint = Paint().also {
+        it.color = Color.GRAY
+        it.strokeWidth = LINE_WIDTH
+        it.style = Paint.Style.STROKE
+    }
+
     override fun draw(canvas: Canvas?) {
         super.draw(canvas)
+        LogUtil.d(this, "draw")
 
         if (canvas == null) return
 
@@ -48,6 +61,7 @@ class TwoScaleGraphView: View {
         maxY = height * 0.05f
 
         drawAxis(canvas)
+        drawHorizontalLines(canvas)
     }
 
     private fun drawAxis(canvas: Canvas) {
@@ -56,5 +70,33 @@ class TwoScaleGraphView: View {
         val yAxis2X = maxX - AXIS_WIDTH / 2
         canvas.drawLine(yAxis2X, oY, yAxis2X, maxY, axisPaint) // yAxis1
         canvas.drawLine(oX, oY, maxX, oY, axisPaint) // xAxis
+    }
+
+    private fun drawHorizontalLines(canvas: Canvas) {
+        val sortedByWeight = records.sortedBy { it.weight }
+        val weightMin = sortedByWeight.firstOrNull()?.weight ?: 0f
+        val weightMax = sortedByWeight.lastOrNull()?.weight ?: 0f
+        val sortedByRate = records.sortedBy { it.rate }
+        val rateMin = sortedByRate.lastOrNull()?.rate ?: 0f
+        val rateMax = sortedByRate.lastOrNull()?.rate ?: 0f
+
+        val weightMinFloor = floor(weightMin)
+        val weightMaxCeil = ceil(weightMax)
+        val weightRange = weightMaxCeil - weightMinFloor
+        val rateMinFloor = floor(rateMin)
+        val rateMaxCeil = ceil(rateMax)
+        val rateRange = rateMaxCeil - rateMinFloor
+
+        if (weightRange > rateRange) {
+            val unitHeight = (oY - maxY) / weightRange
+            for (i in (weightMinFloor.toInt() + 1)..weightMaxCeil.toInt()) {
+                val lineY = oY - unitHeight * (i - weightMinFloor.toInt())
+//                canvas.drawLine(oX, maxX, lineY, lineY, linePaint)
+                canvas.drawLine(oX, lineY, maxX, lineY, linePaint)
+            }
+
+        } else {
+
+        }
     }
 }
