c73c3a93119bd8cdf58840aec499690918ecfe0f
松島勇貴 <macho.macho.dev@gmail.com>
2020-03-10 22:57:29 +0900

    Fix Crashlytics crash: com.google.android.gms.common.api.ApiException: 12501

--- app/src/main/java/org/macho/beforeandafter/preference/backup/BackupPresenter.kt
+++ app/src/main/java/org/macho/beforeandafter/preference/backup/BackupPresenter.kt
@@ -1,6 +1,7 @@
 package org.macho.beforeandafter.preference.backup
 
 import android.accounts.Account
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.util.Log
@@ -48,6 +49,10 @@ class BackupPresenter @Inject constructor(val recordRepository: RecordRepository
     }
 
     override fun result(requestCode: Int, resultCode: Int, data: Intent) {
+        if (resultCode != Activity.RESULT_OK) {
+            return // do nothing
+        }
+
         when (requestCode) {
             RC_SIGN_IN -> {
                 val task = GoogleSignIn.getSignedInAccountFromIntent(data)
